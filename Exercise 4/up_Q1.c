9652df200a6f0c265eb20bb772728b5a8b903248
ShubhamKNIT <121822213+ShubhamKNIT@users.noreply.github.com>
2023-02-14 11:17:46 -0800

    Create up_Q1.c

---
+++ Exercise 4/up_Q1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char s[200];
+    int count = 0, i;
+    printf("Programmer Name: SHUBHAM KUMAR Date:09/02/23\n");
+    printf("AIM: To count the  words in given sentence\n");
+    printf("Enter the string:\n");
+    scanf("%[^\n]s", s);
+    for (i = 0;s[i] != '\0';i++) {
+        if (s[i] == ' ' && s[i+1] != ' ')
+            count++; }
+    printf("Number of words in given string are: %d\n", count + 1); }
